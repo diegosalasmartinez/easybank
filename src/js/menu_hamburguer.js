097ec01c7a3838703728234634a579d1ac4a6e50
diegosalasmartinez <diesalasmart@gmail.com>
2021-02-26 00:19:24 -0500

    mejroando el header

--- src/js/menu_hamburguer.js
+++ src/js/menu_hamburguer.js
@@ -1,12 +1,16 @@
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const menu = document.querySelector('.hamburguer');
 const overlay = document.querySelector('.overlay');
 const popup_menu = document.querySelector('.popup-menu');
 
+console.log(document);
 menu.addEventListener('click',e=>{
     e.preventDefault();
-    
+
     if(header.classList.contains('menu-on')){
+        body.classList.remove('noscroll');
+
         overlay.classList.remove('fade-in');
         overlay.classList.add('fade-out');
         
@@ -16,6 +20,8 @@ menu.addEventListener('click',e=>{
         popup_menu.classList.add('fade-out');
     }
     else{
+        body.classList.add('noscroll');
+
         overlay.classList.remove('fade-out');
         overlay.classList.add('fade-in');
 
